95a6aa14684470a76bcb24fbe7d800c7846da189
Egor Martynov <bigbabyban228@gmail.com>
2025-07-14 03:37:08 +0500

    Made the menu fully adaptive for all device versions and screen sizes. Added a script for the button on the mobile version and smooth transitions

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     const button = document.querySelector('.Button-menu');
-    const menu = document.querySelector('.AppHeader-Menu');
+    const menu = document.querySelector('.AppMain-Menu');
     
     button.addEventListener('click', function() {
         this.classList.toggle('active');
         menu.classList.toggle('active');
+
+        document.body.style.overflow = menu.classList.contains('active') ? 'hidden' : '';
     });
 });
