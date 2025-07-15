6d0ca196db4eadbbc485b7172cb280823fef4798
Egor Martynov <bigbabyban228@gmail.com>
2025-07-15 21:34:31 +0500

    complete

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const button = document.querySelector('.Button-menu');
     const menu = document.querySelector('.AppMain-Menu');
-    const video = document.getElementById('myVideo');
-    const videoContainer = document.querySelector('.video-container');
     
     button.addEventListener('click', function() {
         this.classList.toggle('active');
@@ -11,3 +9,4 @@ document.addEventListener('DOMContentLoaded', function() {
         document.body.style.overflow = menu.classList.contains('active') ? 'hidden' : '';
     });
 });
+
