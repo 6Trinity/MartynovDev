21ad9b7456f87148f7491a5c7510fbf6fd51beeb
Egor Martynov <bigbabyban228@gmail.com>
2025-07-21 08:32:29 +0500

    full refactoring of the site, rewriting of CSS and the structure itself

--- Script/main.js
+++ Script/main.js
@@ -1,13 +1,40 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const button = document.querySelector('.Button-menu');
-    const menu = document.querySelector('.AppMain-Menu');
-    const video = document.getElementById('myVideo');
-    const videoContainer = document.querySelector('.video-container');
+    const button = document.querySelector('.button-menu');
+    const menu = document.querySelector('.appheader-menu-nav');
+    const header = document.getElementById('appheader');
+    const scrollThreshold = 10;
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > scrollThreshold) {
+            header.classList.add('scrolled');
+        } 
+        else {
+            header.classList.remove('scrolled');
+        }
+    });
     
     button.addEventListener('click', function() {
         this.classList.toggle('active');
         menu.classList.toggle('active');
+        
 
         document.body.style.overflow = menu.classList.contains('active') ? 'hidden' : '';
+    });
+});
+
+document.querySelectorAll('.appheader-menu-nav a').forEach(link => {
+    link.addEventListener('click', (e) => {
+
+        document.querySelector('.appheader-menu-nav').classList.remove('active');
+        document.querySelector('.button-menu').classList.remove('active');
+
+        const targetId = link.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
     });
 });
